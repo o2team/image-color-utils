5eb1d383341ffdc455be6cd9b1889c4f6d122ee0
AwesomeDevin <awesomedevin.deng@gmail.com>
2021-03-22 22:23:10 +0800

    feat: 更新d.ts

--- build/index.d.ts
+++ build/index.d.ts
@@ -5,11 +5,26 @@ export interface AdjustConstructor {
   boundaryValue?: number
 }
 
+export interface MediaValue {
+  [key: string]: number[]
+}
+
+export interface MockMoveParams {
+  originColorMedia: MediaValue
+  imageData: ImageData
+  width: number
+}
+
 export declare class ImageColorUtils{
   constructor(params: AdjustConstructor) 
   public compare(oldVal: number[], newVal: number[]): boolean
   public pickColor(imageData: ImageData, x: number, y: number, width: number, type: string): number[]
-  public adjust(imageData: ImageData, width: number ): {x: number, y: number, width: number, height: number} 
+  public pickLineColor (imageData: ImageData, width: number, type?: string[], valueType: string): MediaValue
+
+  public leftTopMockMove ({ originColorMedia, imageData, width }: MockMoveParams): number[]
+  public rightBottomMockMove ({ originColorMedia, imageData, width }: MockMoveParams): number[]
+
+  public adjust(img: HTMLImageElement | ImageBitmap, width: number, height: number): {x: number, y: number, width: number, height: number} 
   public hex2rgb(hex: string): number[] 
   public rgb2hex(rgb: number[]): string
 }
